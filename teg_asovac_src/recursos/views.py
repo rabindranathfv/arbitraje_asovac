52ae2063152359de70dffc3aa92160d76f36b54e
rabindranath <ferreiralox@gmail.com>
2018-06-21 22:14:44 -0400

    add views to test routing in all apps

--- teg_asovac_src/recursos/views.py
+++ teg_asovac_src/recursos/views.py
@@ -4,3 +4,15 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+#import settings
+from django.conf import settings
+# import para envio de correo
+from django.core.mail import send_mail
+
+# Create your views here.
+def recursos_pag(request):
+    context = {
+        "nombre_vista": 'recursos'
+    }
+    return render(request,"test_views.html",context)
