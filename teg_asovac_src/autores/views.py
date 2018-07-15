ce4491e65327e947f25f5ce393ec81e1286b8819
Osvani <osvacolijim@gmail.com>
2018-07-14 23:01:58 -0400

    Hecha vista de autor(vista 1) -Falta realizar la parte del formulario para crear un nuevo trabajo

--- teg_asovac_src/autores/views.py
+++ teg_asovac_src/autores/views.py
@@ -12,4 +12,4 @@ def autores_pag(request):
     context = {
         "nombre_vista": 'autores'
     }
-    return render(request,"test_views.html",context)
+    return render(request,"autor.html",context)
