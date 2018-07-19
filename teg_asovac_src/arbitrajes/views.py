30212ae3a2faa724ac8e936c534f53fbae16d1e1
Rafael V <ravb2013@gmail.com>
2018-07-18 22:03:52 -0400

    Apply code order and quality of life changes
    - Moving "listado_trabajos" and "detalles_resumen" views to "arbitrajes" app.
    - Adding app prefixes to html templates.
    - Adding more and better comments to base_style.css.
    - Fixing a couple of typos.
    - Status labels in "arbitrajes_trabajos_list.html" are bigger and centered.

--- teg_asovac_src/arbitrajes/views.py
+++ teg_asovac_src/arbitrajes/views.py
@@ -15,3 +15,17 @@ def arbitrajes_pag(request):
         "nombre_vista": 'arbitrajes'
     }
     return render(request,"test_views.html",context)
+
+def listado_trabajos(request):
+    context = {
+        'nombre_vista' : 'Lista de Trabajos',
+        'username' : 'Rabindranath Ferreira',
+    }
+    return render(request, 'arbitrajes_trabajos_list.html', context)
+
+def detalles_resumen(request):
+    context = {
+        'nombre_vista' : 'Detalles de Resumen',
+        'username' : 'Rabindranath Ferreira',
+    }
+    return render(request, 'arbitrajes_detalle_resumen.html', context)
