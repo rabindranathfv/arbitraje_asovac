663bf4dbead6498f6c012256dbcd35f98f4cf463
Rabindranath Ferreira <ferreiralox@gmail.com>
2018-07-21 22:34:47 -0400

    add constancia_estudio in autores model

--- teg_asovac_src/autores/models.py
+++ teg_asovac_src/autores/models.py
@@ -37,7 +37,7 @@ class Autor(models.Model):
 	correo_electronico = models.EmailField(max_length=254)
 	telefono_oficina = models.CharField(max_length=20)
 	telefono_habitacion_celular= models.CharField(max_length=20)
-	# Constancia de estudio no se sabe qué será
+	constancia_estudio= models.CharField(max_length=255,blank=True)
 	direccion_envio_correspondencia = models.TextField(max_length=100,blank=True)
 	es_miembro_asovac = models.BooleanField(default=False)
 	capitulo_perteneciente = models.CharField(max_length=20,blank=True)
