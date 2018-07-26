982d91dd5ac92e6a9728682e82ca3a7d16ecbbc6
Rabindranath Ferreira <ferreiralox@gmail.com>
2018-07-25 23:52:40 -0400

    Update trabajo model

--- teg_asovac_src/trabajos/models.py
+++ teg_asovac_src/trabajos/models.py
@@ -24,10 +24,11 @@ class Trabajo(models.Model):
 	subarea2 = models.CharField(max_length=20)
 	subarea3 = models.CharField(max_length=20)
 	resumen = models.TextField(max_length=255,default='')
-	documento_inscrito=  models.CharField(max_length=100,blank=True)
+	documento_inscrito =  models.CharField(max_length=100,blank=True)
 	observaciones = models.TextField(max_length=255, blank = True)
 	url_trabajo = models.CharField(max_length=255,blank=True)
-	version= models.CharField(max_length=20,blank=True)
+	version = models.CharField(max_length=20,blank=True)
+	# add field to storage work in pdf
 	def __str__(self):
     		return self.titulo_espanol#.encode('utf-8', errors='replace')
 
