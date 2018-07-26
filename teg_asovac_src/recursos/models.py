dd7e1bc547f6826e50890535d3cea728f7f4d46e
Rabindranath Ferreira <ferreiralox@gmail.com>
2018-07-25 23:50:44 -0400

    Separete models with space in Recursos model

--- teg_asovac_src/recursos/models.py
+++ teg_asovac_src/recursos/models.py
@@ -7,10 +7,10 @@ from django.db import models
 
 class Recurso(models.Model):
 
-    usuario_asovac= models.ManyToManyField('main_app.Usuario_asovac')
+    usuario_asovac = models.ManyToManyField('main_app.Usuario_asovac')
 
-    nombre= models.CharField(max_length=100)
-    descripcion= models.CharField(max_length=200)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=200)
 
     def __str__(self):
         return self.nombre 
