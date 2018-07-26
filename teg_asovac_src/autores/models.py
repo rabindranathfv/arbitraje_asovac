9bd0b1003e95711e90798e49b230fc81861ef4db
Rabindranath Ferreira <ferreiralox@gmail.com>
2018-07-25 23:48:49 -0400

    Update nombres,apellido to singular time in Autor model

--- teg_asovac_src/autores/models.py
+++ teg_asovac_src/autores/models.py
@@ -30,8 +30,8 @@ class Autor(models.Model):
 	Sistema_asovac_id = models.ManyToManyField('main_app.Sistema_asovac')
 
 	marca_temporal = models.DateTimeField(auto_now=True)
-	nombres = models.CharField(max_length=40)
-	apellidos = models.CharField(max_length=40)
+	nombre = models.CharField(max_length=40)
+	apellido = models.CharField(max_length=40)
 	genero = models.CharField(max_length=1)
 	cedula_pasaporte = models.CharField(max_length=20)
 	correo_electronico = models.EmailField(max_length=254)
