7561350868e27928496ee02b6575fb7105e3c00a
Osvani Colina <osvacolijim@gmail.com>
2018-07-31 18:37:10 -0400

    Done state 6

--- teg_asovac_src/sesiones/views.py
+++ teg_asovac_src/sesiones/views.py
@@ -11,7 +11,7 @@ from main_app.models import Rol,Sistema_asovac,Usuario_asovac
 # Global functions
 # Esta función verifica que se va a desplegar la opción de configuracion general en el sidebar, retorna 1 si se usará y 0 sino.
 def verify_configuracion_general_option(estado, rol_id, item_active): 
-    if ((estado == '0' or estado =='1' or estado =='3' or estado =='5') and 1 in rol_id and item_active == 1) or ((estado =='2' or estado == '4') and (1 in rol_id or 2 in rol_id) and item_active == 1):
+    if ((estado == '0' or estado =='1' or estado =='3' or estado =='5' or estado =='6') and 1 in rol_id and item_active == 1) or ((estado =='2' or estado == '4') and (1 in rol_id or 2 in rol_id) and item_active == 1):
         return 1
     return 0
 
@@ -27,19 +27,19 @@ def verify_estado_arbitrajes_option(estado,rol_id,item_active):
 
 
 def verify_usuario_option(estado,rol_id, item_active):
-    if ((estado == '0' or estado =='1' or estado =='2' or estado == '3' or estado == '4' or estado == '5') and 1 in rol_id and item_active == 1):
+    if ((estado == '0' or estado =='1' or estado =='2' or estado == '3' or estado == '4' or estado == '5' or estado == '6') and 1 in rol_id and item_active == 1):
         return 1
     return 0
 
 
 def verify_asignacion_coordinador_general_option(estado,rol_id,item_active):
-    if((estado == '1' or estado =='2' or estado == '3' or estado == '4' or estado == '5') and  1 in rol_id and item_active == 1):
+    if((estado == '1' or estado =='2' or estado == '3' or estado == '4' or estado == '5' or estado =='6') and  1 in rol_id and item_active == 1):
         return 1
     return 0
 
 
 def verify_asignacion_coordinador_area_option(estado,rol_id,item_active):
-    if(estado =='2' and  (1 in rol_id or 2 in rol_id) and item_active ==1) or((estado == '3' or estado == '4' or estado == '5') and 1 in rol_id and item_active == 1):
+    if(estado =='2' and  (1 in rol_id or 2 in rol_id) and item_active ==1) or((estado == '3' or estado == '4' or estado == '5' or estado =='6') and 1 in rol_id and item_active == 1):
         return 1
     return 0
 
@@ -47,43 +47,43 @@ def verify_asignacion_coordinador_area_option(estado,rol_id,item_active):
 
 
 def verify_recursos_option(estado,rol_id,item_active):
-    if ((estado == '0' or estado == '1' or estado == '2' or estado == '3' or estado == '4' or estado == '5') and 1 in rol_id and item_active == 1):
+    if ((estado == '0' or estado == '1' or estado == '2' or estado == '3' or estado == '4' or estado == '5' or estado =='6') and 1 in rol_id and item_active == 1):
         return 1
     return 0
 
 def verify_areas_subareas_option(estado,rol_id,item_active):
-    if ((estado == '0' or estado =='3' or estado == '4' or estado == '5') and 1 in rol_id and item_active == 1) or (estado == '1' and (1 in rol_id or 2 in rol_id or 3 in rol_id) and item_active == 1) or (estado == '2' and (1 in rol_id or 2 in rol_id) and item_active ==1):
+    if ((estado == '0' or estado =='3' or estado == '4' or estado == '5' or estado =='6') and 1 in rol_id and item_active == 1) or (estado == '1' and (1 in rol_id or 2 in rol_id or 3 in rol_id) and item_active == 1) or (estado == '2' and (1 in rol_id or 2 in rol_id) and item_active ==1):
         return 1
     return 0
 
 
 def verify_autores_option(estado,rol_id,item_active):
-    if ((estado == '0' or estado =='5') and 1 in rol_id and item_active == 2) or ((estado == '1' or estado =='2' or estado == '3' or estado == '4') and (1 in rol_id or 2 in rol_id) and item_active == 2):
+    if ((estado == '0' or estado =='5' or estado =='6') and 1 in rol_id and item_active == 2) or ((estado == '1' or estado =='2' or estado == '3' or estado == '4') and (1 in rol_id or 2 in rol_id) and item_active == 2):
         return 1
     return 0
 
 def verify_arbitros_option(estado,rol_id, item_active):
-    if ((estado == '0' or estado =='5') and 1 in rol_id and item_active == 2) or ((estado == '1' or estado =='2' or estado == '3' or estado == '4') and (1 in rol_id or 2 in rol_id or 3 in rol_id) and item_active == 2):
+    if ((estado == '0' or estado =='5' or estado =='6') and 1 in rol_id and item_active == 2) or ((estado == '1' or estado =='2' or estado == '3' or estado == '4') and (1 in rol_id or 2 in rol_id or 3 in rol_id) and item_active == 2):
         return 1
     return 0
 
 def verify_sesions_arbitraje_option(estado,rol_id, item_active):
-    if ((estado == '0' or estado =='1' or estado == '2' or estado == '3' or estado =='5') and 1 in rol_id and item_active == 2) or (estado == '4' and (1 in rol_id or 2 in rol_id or 3 in rol_id) and item_active == 2):
+    if ((estado == '0' or estado =='1' or estado == '2' or estado == '3' or estado =='5' or estado =='6') and 1 in rol_id and item_active == 2) or (estado == '4' and (1 in rol_id or 2 in rol_id or 3 in rol_id) and item_active == 2):
         return 1
     return 0
 
 def verify_arbitraje_option(estado,rol_id, item_active):
-    if ((estado == '0' or estado =='1' or estado == '2' or estado == '3' or estado == '4' or estado =='5') and 1 in rol_id and item_active == 2):
+    if ((estado == '0' or estado =='1' or estado == '2' or estado == '3' or estado == '4' or estado =='5') and 1 in rol_id and item_active == 2) or (estado =='6' and (1 in rol_id or 2 in rol_id or 3 in rol_id) and item_active == 2):
         return 1
     return 0
 
 def verify_trabajo_option(estado, rol_id,item_active):
-    if((estado =='3' or estado =='4' or estado =='5')and (1 in rol_id or 2 in rol_id or 3 in rol_id) and item_active == 2):
+    if((estado =='3' or estado =='4' or estado =='5')and (1 in rol_id or 2 in rol_id or 3 in rol_id) and item_active == 2) or (estado =='6' and 1 in rol_id and item_active ==2):
         return 1
     return 0
 
 def verify_eventos_sidebar_full(estado,rol_id,item_active):
-    if ((estado == '0' or estado =='1' or estado == '2' or estado == '3' or estado == '4' or estado =='5') and 1 in rol_id and item_active == 4):
+    if ((estado == '0' or estado =='1' or estado == '2' or estado == '3' or estado == '4' or estado =='5' or estado =='6') and 1 in rol_id and item_active == 4):
         return 1
     return 0
 
