d9d5097ab4bfa49d3e5bde159eeb0f5fc7fa0924
Rabindranath <ferreiralox@gmail.com>
2018-08-24 01:10:33 -0300

    change genero model type, delete m2m organizer - Sistema_asovac

--- teg_asovac_src/eventos/models.py
+++ teg_asovac_src/eventos/models.py
@@ -12,13 +12,10 @@ Organizador Model
 class Organizador(models.Model):
 
 	usuario_asovac = models.OneToOneField('main_app.Usuario_asovac')
-	# cambiar a seleccion multiple 
-	Sistema_asovac_id = models.ManyToManyField('main_app.Sistema_asovac')
 
 	nombres = models.CharField(max_length=50)
 	apellidos = models.CharField(max_length=50)
-	#cambiar a small integer
-	genero = models.CharField(max_length=5)
+	genero = models.SmallIntegerField()
 	cedula_o_pasaporte = models.CharField(max_length=20)
 	correo_electronico = models.EmailField(max_length=254)
 	institucion = models.CharField(max_length=50)
