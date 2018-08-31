8ff38db8e1389786974bcddf558a066efb9ab37a
Rabindranath <ferreiralox@gmail.com>
2018-08-31 19:02:46 -0300

    add prints in validators

--- teg_asovac_src/eventos/validators.py
+++ teg_asovac_src/eventos/validators.py
@@ -4,16 +4,20 @@ from django.utils.translation import gettext_lazy as _
 
 def validate_ced_passport(data):
     dni_data = data
+    print("validando cedula o pasaporte")
     return data
 
 def validate_phone_office(phone):
     phone_data = phone
+    print("validando telefono de oficina")
     return phone
 
 def validate_phone_personal(phone_per):
     phone_personal_data = phone_per
+    print("validando telefono personal")
     return phone_per
 
 def validate_cap_asovac(cap_asovac):
     cap_asovac_data = cap_asovac
+    print("validando cap_asovac")
     return cap_asovac
