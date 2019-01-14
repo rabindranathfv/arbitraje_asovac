590765d4faf9db983aa2c298f011e4974d4a153a
Albert <johan50693@gmail.com>
2019-01-13 20:30:49 -0400

    add config initial for user admin

--- teg_asovac_src/main_app/models.py
+++ teg_asovac_src/main_app/models.py
@@ -135,8 +135,120 @@ def crear_usuario_asovac(sender, **kwargs):
 
 		first_usuario_asovac = Usuario_asovac.objects.count()
 		roles = Rol.objects.count()
+		list_areas= Area.objects.count()
+		
 
-		if first_usuario_asovac == 1 and roles == 0: #Esto asegura la creación de roles al momento de crear el primer usuario
+		if list_areas == 0:
+    		# Para cargar contenido para el area 1
+			area= Area(id=1,nombre="Biociencias",descripcion="-")
+			area.save()
+
+			sub_area= Sub_area(nombre="Parasitología",descripcion="-",area_id=1)
+			sub_area.save()
+
+			sub_area= Sub_area(nombre="Biología Molecular",descripcion="-",area_id=1)
+			sub_area.save()
+
+			sub_area= Sub_area(nombre="Bioquímica",descripcion="-",area_id=1)
+			sub_area.save()
+
+			sub_area= Sub_area(nombre="Tecnología de Alimentos",descripcion="-",area_id=1)
+			sub_area.save()
+
+			sub_area= Sub_area(nombre="Ecología",descripcion="-",area_id=1)
+			sub_area.save()
+
+			sub_area= Sub_area(nombre="Medicina",descripcion="-",area_id=1)
+			sub_area.save()
+
+			sub_area= Sub_area(nombre="Inmunología",descripcion="-",area_id=1)
+			sub_area.save()
+
+			sub_area= Sub_area(nombre="Microbiología",descripcion="-",area_id=1)
+			sub_area.save()
+
+			sub_area= Sub_area(nombre="Farmacología",descripcion="-",area_id=1)
+			sub_area.save()
+
+    		# Para cargar contenido para el area 2
+			area= Area(id=2,nombre="Ciencias Exactas",descripcion="-")
+			area.save()
+
+			sub_area= Sub_area(nombre="Matemática",descripcion="-",area_id=2)
+			sub_area.save()
+
+			sub_area= Sub_area(nombre="Química Organometálica",descripcion="-",area_id=2)
+			sub_area.save()
+
+			sub_area= Sub_area(nombre="Productos Naturales",descripcion="-",area_id=2)
+			sub_area.save()
+
+			sub_area= Sub_area(nombre="Farmacología",descripcion="-",area_id=2)
+			sub_area.save()
+
+			sub_area= Sub_area(nombre="Nanosistemas",descripcion="-",area_id=2)
+			sub_area.save()
+
+    		# Para cargar contenido para el area 3
+			area= Area(id=3,nombre="Tecnología",descripcion="-")
+			area.save()
+
+			sub_area= Sub_area(nombre="Computación",descripcion="-",area_id=3)
+			sub_area.save()
+
+			sub_area= Sub_area(nombre="Ingeniería Electrónica y Biomédica",descripcion="-",area_id=3)
+			sub_area.save()
+
+    		# Para cargar contenido para el area 4
+			area= Area(id=4,nombre="Ciencias Sociales",descripcion="-")
+			area.save()
+
+			sub_area= Sub_area(nombre="Ciencias Sociales",descripcion="-",area_id=4)
+			sub_area.save()
+
+			sub_area= Sub_area(nombre="Educación",descripcion="-",area_id=4)
+			sub_area.save()
+
+			# Para cargar contenido para el area 5
+			area= Area(id=5,nombre="Congreso de la Sociedad Venezolana de Física",descripcion="-")
+			area.save()
+
+			sub_area= Sub_area(nombre="Física General",descripcion="-",area_id=5)
+			sub_area.save()
+
+			sub_area= Sub_area(nombre="Óptica y Plasmas",descripcion="-",area_id=5)
+			sub_area.save()
+
+			sub_area= Sub_area(nombre="Astronomía",descripcion="-",area_id=5)
+			sub_area.save()
+
+			sub_area= Sub_area(nombre="Física de Partículas Virtual",descripcion="-",area_id=5)
+			sub_area.save()
+
+			sub_area= Sub_area(nombre="Cosmología",descripcion="-",area_id=5)
+			sub_area.save()
+
+			sub_area= Sub_area(nombre="Materia Condensada Teórica",descripcion="-",area_id=5)
+			sub_area.save()
+
+			sub_area= Sub_area(nombre="Instrumentación",descripcion="-",area_id=5)
+			sub_area.save()
+
+			sub_area= Sub_area(nombre="Física Médica",descripcion="-",area_id=5)
+			sub_area.save()
+
+			sub_area= Sub_area(nombre="Física Nuclear",descripcion="-",area_id=5)
+			sub_area.save()
+
+			sub_area= Sub_area(nombre="Física de los Materiales",descripcion="-",area_id=5)
+			sub_area.save()
+			
+			sub_area= Sub_area(nombre="Física del Espacio",descripcion="-",area_id=5)
+			sub_area.save()
+
+
+		#Esto asegura la creación de roles al momento de crear el primer usuario
+		if first_usuario_asovac == 1 and roles == 0: 
 			rol = Rol(nombre="Admin", descripcion="Administrador")
 			rol.save()
 
@@ -154,10 +266,14 @@ def crear_usuario_asovac(sender, **kwargs):
 
 			Rol_admin=Usuario_rol_in_sistema(status="1",rol_id="1",usuario_asovac_id=usuario_asovac.id)
 			Rol_admin.save()
+			usuario_asovac.sub_area.add(Sub_area.objects.get(id=1))
+			usuario_asovac.save()
 		else:
 			if first_usuario_asovac == 1:
 				Rol_admin=Usuario_rol_in_sistema(status="1",rol_id="1",usuario_asovac_id=usuario_asovac.id)
 				Rol_admin.save()
+				usuario_asovac.sub_area.add(Sub_area.objects.get(id=1))
+				usuario_asovac.save()
 
 post_save.connect(crear_usuario_asovac, sender=User)
 
