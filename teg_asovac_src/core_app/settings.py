33ee0cc5b9a721512f7c48d63c80f99eec449e71
osvanicolina <osvacolijim@gmail.com>
2019-06-03 23:53:45 -0400

    Changing port from string to int

--- teg_asovac_src/core_app/settings.py
+++ teg_asovac_src/core_app/settings.py
@@ -130,7 +130,7 @@ DATABASES['default'].update(db_from_env)
 
 EMAIL_USE_TLS = os.environ['EMAIL_USE_TLS']
 EMAIL_HOST = os.environ['EMAIL_HOST']
-EMAIL_PORT = os.environ['EMAIL_PORT']
+EMAIL_PORT = int(os.environ['EMAIL_PORT'])
 EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
