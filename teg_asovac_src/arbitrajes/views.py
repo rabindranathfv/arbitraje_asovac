f7183f14cc44e337eb252b40e24e51876f8b5dfd
Rafael V <ravb2013@gmail.com>
2019-08-08 13:33:25 -0400

    Refactor and add tooltip to referees table template
    - Refactoring the referees_list and session asignment templates by removing
    unnecessary commented html and organizing remaining html rows.
    - Making the arbitrajes/views.py/referee_list function PEP8 compliant.
    - Adding a small tooltip to the download button in the referees list table.

--- teg_asovac_src/arbitrajes/views.py
+++ teg_asovac_src/arbitrajes/views.py
@@ -179,16 +179,16 @@ def referee_list(request):
     arbitraje_id = request.session['arbitraje_id']
     arbitraje = Sistema_asovac.objects.get(pk=arbitraje_id)
     estado = arbitraje.estado_arbitraje
-    rol_id=get_roles(request.user.id,arbitraje_id)
+    rol_id = get_roles(request.user.id, arbitraje_id)
 
     item_active = 2
-    items=validate_rol_status(estado,rol_id,item_active, arbitraje_id)
+    items = validate_rol_status(estado, rol_id, item_active, arbitraje_id)
 
-    route_conf= get_route_configuracion(estado,rol_id, arbitraje_id)
-    route_seg= get_route_seguimiento(estado,rol_id)
-    route_trabajos_sidebar = get_route_trabajos_sidebar(estado,rol_id,item_active)
-    route_trabajos_navbar = get_route_trabajos_navbar(estado,rol_id)
-    route_resultados = get_route_resultados(estado,rol_id, arbitraje_id)
+    route_conf = get_route_configuracion(estado, rol_id, arbitraje_id)
+    route_seg = get_route_seguimiento(estado, rol_id)
+    route_trabajos_sidebar = get_route_trabajos_sidebar(estado, rol_id, item_active)
+    route_trabajos_navbar = get_route_trabajos_navbar(estado, rol_id)
+    route_resultados = get_route_resultados(estado, rol_id, arbitraje_id)
 
     context = {
         'nombre_vista' : 'Listado de Arbitros',
