17865937298befb4e20cbd77fcda452614082ef8
Rafael V <ravb2013@gmail.com>
2019-08-13 23:25:16 -0400

    Render users by roles graph in dashboard
    - Creating a dashboardCharts class in utils.py to return the correct data
    when it receives a query.
    - Adding ajax query to render the graphs in the dashboard for every
    sistema_asovac.
    - Adding a url to query the dashboard graph data.
    - Adding small changes to strings and comments.

---
+++ teg_asovac_src/main_app/utils.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Usuario_rol_in_sistema, Rol, Sistema_asovac, Usuario_asovac
+
+class DashboardCharts(APIView):
+    authentication_classes = ()
+    permission_classes = ()
+
+    def get(self, request, format=None):
+        arbitraje_id = request.session['arbitraje_id']
+        #El siguiente codigo es para obtener los datos necesarios para la grafica de usuarios
+        #distribuidos por roles
+        coord_area_count = Usuario_rol_in_sistema.objects.filter(rol=3,
+                                                                 sistema_asovac=arbitraje_id,
+                                                                 status=True).count()
+        referee_count = Usuario_rol_in_sistema.objects.filter(rol=4,
+                                                              sistema_asovac=arbitraje_id,
+                                                              status=True).count()
+        author_count = Usuario_rol_in_sistema.objects.filter(rol=5,
+                                                             sistema_asovac=arbitraje_id,
+                                                             status=True).count()
+
+
+        data = {
+            "usuarios": {
+                "labels": ["Coordinadores de Área", "Árbitros", "Autores"],
+                "data": [coord_area_count, referee_count, author_count]
+            },
+            # "arbitros": {
+            #     "labels": ['Total árbitros', 'Invitaciones aceptadas', 'Invitaciones pendientes'],
+            #     "data": [total_arbitros, invitaciones_aceptadas, invitaciones_pendientes]
+            # },
+            # "autores": {
+            #     "labels": ['Educación Básica Primaria', 'Educación Básica Secundaria', 'Bachillerato/Educación Media', 'Educación Técnico/Profesional', 'Universidad', 'Postgrado'],
+            #     "data": [educacion_primaria, educacion_secundaria, bachillerato, tecnico, universidad, postgrado]
+            # },
+            # "area":{
+            #     "labels": areas_labels_splited,
+            #     "data_aceptados": areas_trabajos_aceptados,
+            #     "data_rechazados": areas_trabajos_rechazados,
+            # },
+            # "autoresArea":{
+            #     "labels": areas_labels,
+            #     "data": areas_autores,
+            #     "colores": areas_colores
+            # },
+        }
+        return Response(data)
