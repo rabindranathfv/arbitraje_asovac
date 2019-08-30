bb872253246911f69864d1ab117f6c9683ed2c8e
osvanicolina <osvacolijim@gmail.com>
2019-08-30 15:26:26 -0400

    Change load_authors modal into a generic modal

--- teg_asovac_src/autores/views.py
+++ teg_asovac_src/autores/views.py
@@ -1184,8 +1184,10 @@ def load_authors_modal(request):
 
 	context ={
 		'form': form,
+		'rol': 'autores',
+		'action': reverse('autores:load_authors_modal')
 	}
-	data['html_form'] = render_to_string('ajax/load_authors.html', context, request=request)
+	data['html_form'] = render_to_string('ajax/load_excel.html', context, request=request)
 	return JsonResponse(data)
 
 
